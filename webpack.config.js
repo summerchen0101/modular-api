2e38ceba8f1fc1d7a4062ff1469552853b93b879
Summer <mp4bj6ruby@gmail.com>
2020-04-20 10:46:45 +0800

    commonjs

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = [
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist'),
-      libraryTarget: 'commonjs2',
+      libraryTarget: 'commonjs',
     },
   },
   {
