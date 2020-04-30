c6db6542805db763ae3fddd2f9fd4fdad233838e
Summer <mp4bj6ruby@gmail.com>
2020-04-30 15:14:09 +0800

    調整gitignore

--- dist/default.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    type: 'json'
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVmYXVsdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9kZWZhdWx0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsa0JBQWU7SUFDYixJQUFJLEVBQUUsTUFBTTtDQUNHLENBQUEifQ==
